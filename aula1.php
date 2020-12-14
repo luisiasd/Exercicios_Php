536cbb3481e786d96c6c8ef2df0758b4dcb41c6c
Luis Eduardo <72215228+luisiasd@users.noreply.github.com>
2020-12-13 22:05:53 -0300

    Update aula1.php
    indentação

--- aula1.php
+++ aula1.php
@@ -1,33 +1,33 @@
 <!DOCTYPE html>
 <html lang="pt">
 <head>
-				<link rel="stylesheet" type="text/css" href="estilo.css" media="screen" />				
-				<title>Título da página</title>
+	<link rel="stylesheet" type="text/css" href="estilo.css" media="screen" />				
+	<title>#1</title>
 
 </head>
 <body>
-		<section>
-						<div class="container">
-								<h1>cod. Votação</h1>
-								<?php
-								$ano   = $_GET["idade"];
-								$idade = 2020 - $ano;
-								echo "Quem nasceu em $ano tem $idade";
-								$tipo = ($idade >= 18 && $idade <= 65) ? "<h5>Obrigatorio!</h5>" : "Não Obrigatorio!";
-								echo " e dessa forma seu voto é $tipo";
-								?>
+	<section>
+		<div class="container">
+			<h1>cod. Votação</h1>
+			<?php
+				$ano   = $_GET["idade"];
+				$idade = 2020 - $ano;
+				echo "Quem nasceu em $ano tem $idade";
+				$tipo = ($idade >= 18 && $idade <= 65) ? "<h5>Obrigatorio!</h5>" : "Não Obrigatorio!";
+				echo " e dessa forma seu voto é $tipo";
+			?>
 
-								<div class="rodape">
-												<h6>exercicios</h6>
-												<a href="index.php">0</a>
-												<a href="aula1.php">1</a>
-												<a href="aula2.php">2</a>
-												<a href="aula3.html">3</a>
-												<a href="aula4.html">4</a>
-											 <a here="aula5.html">5</a>
-											 <a href="aula6.html">6</a>
-								</div>
-						</div>						
-		</section>
+		<div class="rodape">
+			<h6>exercicios</h6>
+			<a href="index.php">0</a>
+			<a href="aula1.php">1</a>
+			<a href="aula2.php">2</a>
+			<a href="aula3.html">3</a>
+			<a href="aula4.html">4</a>
+			<a here="aula5.html">5</a>
+			<a href="aula6.html">6</a>
+		</div>
+		</div>						
+	</section>
 </body>
 </html>
