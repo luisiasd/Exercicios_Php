e6d3add366d7dee980ad8aa202f573f87f1e0a65
Luis Eduardo <72215228+luisiasd@users.noreply.github.com>
2020-12-16 08:12:29 -0300

    Update form.php

--- form.php
+++ form.php
@@ -1,42 +1,41 @@
 <a href=""></a><!DOCTYPE html>
 <html lang="pt">
 <head>
-				<link rel="stylesheet" type="text/css" href="estilo.css" media="screen" />				
-				<title>Título da página</title>
-
+	<link rel="stylesheet" type="text/css" href="estilo.css" media="screen" />				
+	<title>Título da página</title>
 </head>
 <body>
-		<section>
-						<div class="container">
-								<h1>Dados do Formulario</h1>
-								<?php
-								$nome           = $_POST["nome"];
-								$sobrenome      = $_POST["sobrenome"];
-								$sexo           = $_POST["sexo"];
-								$dataNascimento = $_POST["dataNascimento"];
-								$senha          = $_POST["senha"];
-								echo "$nome $sobrenome $sexo $dataNascimento $senha";
-								?>
-								<h1>Estilização de Texto</h1>
-								<?php
-								$texto   = $_POST["t"]   ? $_POST["t"]   : "Vazio";
-								$tamanho = $_POST["tam"] ? $_POST["tam"] : "Vazio";
-								$cor     = $_POST["cor"] ? $_POST["cor"] : "Vazaio";
-								?>
-								<?php 
-								echo "<span class='texto'>$texto</span>";
-								?>
-								<div class="rodape">
-												<h6>exercicios</h6>
-												<a href="index.php">0</a>
-												<a href="aula1.php">1</a>
-												<a href="aula2.php">2</a>
-												<a href="aula3.html">3</a>
-												<a href="aula4.html">4</a>
-											 <a here="aula5.html">5</a>
-											 <a href="aula6.html">6</a>
-								</div>
-						</div>
-		</section>
+	<section>
+		<div class="container">
+			<h1>Dados do Formulario</h1>
+			<?php
+				$nome           = $_POST["nome"];
+				$sobrenome      = $_POST["sobrenome"];
+				$sexo           = $_POST["sexo"];
+				$dataNascimento = $_POST["dataNascimento"];
+				$senha          = $_POST["senha"];
+				echo "$nome $sobrenome $sexo $dataNascimento $senha";
+			?>
+			<h1>Estilização de Texto</h1>
+			<?php
+				$texto   = $_POST["t"]   ? $_POST["t"]   : "Vazio";
+				$tamanho = $_POST["tam"] ? $_POST["tam"] : "Vazio";
+				$cor     = $_POST["cor"] ? $_POST["cor"] : "Vazaio";
+			?>
+			<?php 
+				echo "<span class='texto'>$texto</span>";
+			?>
+			<div class="rodape">
+				<h6>exercicios</h6>
+				<a href="index.php">0</a>
+				<a href="aula1.php">1</a>
+				<a href="aula2.php">2</a>
+				<a href="aula3.html">3</a>
+				<a href="aula4.html">4</a>
+				<a here="aula5.html">5</a>
+				 <a href="aula6.html">6</a>
+			</div>
+		</div>
+	</section>
 </body>
 </html>
